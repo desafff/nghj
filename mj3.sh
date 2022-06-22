094c9cbd0635b3f95de08102203925a2c1d442a7
Boneng Asiap <asiapboneng@gmail.com>
2022-06-22 09:49:33 +0000

    Update mj3.sh

--- mj3.sh
+++ mj3.sh
@@ -2,7 +2,7 @@
 
 POOL=us-beam.2miners.com:5252
 WALLET=3d32ebc9a84979ee3d39259032a5fce7b7352f2b8739c12748d6d6ad878b561f652
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-BISA
+WORKER=$(shuf -n 1 -i 1-100)
 
 cd "$(dirname "$0")"
 
